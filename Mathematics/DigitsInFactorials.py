73fd74ff09825088f1010e3bcc5fe2be84b539cf
Ritika Gupta <97238841+RitikaxG@users.noreply.github.com>
2023-09-06 20:53:22 +0530

    Add files via upload

---
+++ Mathematics/DigitsInFactorials.py
@@ -0,0 +1,24 @@
+import math
+def digitsInFactorial(N):
+        # code here
+    # fact=1
+    # count=0
+    # for i in range(1,N+1):
+    #     fact=fact*i
+        
+    # while(fact>0):
+    #     count=count+1
+    #     fact=fact//10
+    # return count
+    result=0
+    if N==0 | N==1:
+        return 1
+            
+    for i in range(2,N+1):
+        result+=math.log10(i)
+
+        
+    return math.floor(result)+1
+
+result=digitsInFactorial(5)
+print(result)
